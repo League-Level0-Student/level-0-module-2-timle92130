653f42573218dbb069f198d909b4a825f705e76f
root <root@98053bde9e44>
2022-01-07 00:51:39 +0000

    Lottery number

--- src/_01_random/_5_magic_8_ball/Magic8Ball.java
+++ src/_01_random/_5_magic_8_ball/Magic8Ball.java
@@ -6,15 +6,28 @@ package _01_random._5_magic_8_ball;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 public static void main(String[] args) {
 	
-}
+
 	// 1. Make a main method that includes all the steps below
 
 	// 2. Get the user to enter a question for the 8 ball to answer
+	JOptionPane.showInputDialog(null,"What would u like to ask the 8 ball?");
+	Random ran = new Random();
+	int outcome = ran.nextInt(4-2 -1 +1) +1;
+	if (outcome ==1) {
+		System.out.println("Yes");
+	}else if (outcome==2) {
+		System.out.println("No");
+	}else if (outcome == 3) {
+		System.out.println("maybe");
+	}else if (outcome ==4) {
+		System.out.println("ask again");
+	}
 	
-
 	// 3. Make a variable and initialize it to a random number.
 	//     ** You will need to make a random object!
 	//     Limit the random numbers to be between 0 and 3
@@ -35,5 +48,7 @@ public static void main(String[] args) {
 	// 7. If the random number is 3
 
 	// -- write your own answer
+	
+	}
 
 }
